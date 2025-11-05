bf7bfc1db86eff9f3d129eb072d89cea7f1119d1
Alex S <Alexander2.Silvester@live.uwe.ac.uk>
2025-11-05 15:24:30 +0000

    player can get in the forklift and use the camera
    the player can now use the forklift with the new controls and move the camera around

--- Project/Assets/_Data/Scripts/Player/PlayerController.cs
+++ Project/Assets/_Data/Scripts/Player/PlayerController.cs
@@ -1,136 +1,89 @@
+using StarterAssets;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 using UnityEngine.Windows;
+using static Unity.Burst.Intrinsics.X86;
+using static UnityEditor.PlayerSettings;
 
 // This script is for our game's custom features
 // The other FirstPersonController is for first person movement code
 public class PlayerController : MonoBehaviour
 {
 	[Header("Settings")]
-    [SerializeField][Range(1, 4)] private int playerNumber = 1;
-	//[SerializeField] [Min(0)] private float vehicleEnterDistance = 1f;
-	// Hold for this many seconds to enter vehicle (like Halo)
-	// Also prevents issue where player exiting gets back in straight away (because they are holding button)
+	[SerializeField][Range(1, 4)] private int playerNumber = 1;
 	[SerializeField] private float enterVehicleTime = 0.5f;
-	[SerializeField] private float speed = 1f;
-	
-	[Header("UI")]
-	[SerializeField] private HudManager hudManager;
-	
+
+	//[Header("UI")]
+	//[SerializeField] private HudManager hudManager;
+
 	// Enter / exiting vehicles
 	private List<IDriveable> driveablesInRange = new List<IDriveable>();
-	private float currentEnterVehicleTimer = 0;
-	
-	// Cache
-	private CharacterController controller;
 
-	[SerializeField] GameObject camera_manager;
+	[Space(20)]
+	[Header("Player Model")]
+	[SerializeField] GameObject model;
 
-    [Tooltip("Animator component for the player model")]
-    public Animator anim;
+	private ForkliftController current_forklift = null;
+
+	[SerializeField] GameObject camera;
+
+	[SerializeField][Range(0, 1)] float max_rotation = 0.3f;
+
+	public bool driving { get; private set; }
 
     private void Start()
 	{
-		controller = GetComponent<CharacterController>();
-	}
-	
-	private void OnEnable()
+        driving = false;
+    }
+
+    private void OnEnable()
 	{
 		// Reset
 		driveablesInRange = new List<IDriveable>();
-		currentEnterVehicleTimer = 0;
-	}
-	
-	private void Update()
-	{
-		CameraRotation();
-		move();
-
-		// Is the player trying to enter a vehicle?
-		if (UnityEngine.Input.GetButton("Mouse Left" + playerNumber))
-		{
-			if (currentEnterVehicleTimer >= enterVehicleTime)
-			{
-				camera_manager.GetComponent<CameraManager>().changePerspective3rd(playerNumber);
-
-				if (TryEnterVehicleInRange())
-				{
-					hudManager.SetVehiclePromptStatus(playerNumber, false);
-					gameObject.SetActive(false);
-				}
-			}
-			else
-			{
-				if (currentEnterVehicleTimer == 0)
-				{
-					hudManager.SetVehiclePromptText(playerNumber, "Drive Forklift");
-				}
-				
-				currentEnterVehicleTimer += Time.deltaTime;
-			}
-		}
-		else
-		{
-			currentEnterVehicleTimer = 0;
-		}
-		
-															// Calculate percentage complete
-		hudManager.SetVehiclePromptProgress(playerNumber, (currentEnterVehicleTimer / enterVehicleTime));
 	}
 
-    private void move()
+    private void OnDisable()
     {
-		Vector3 inputDirection = new Vector3(0,0,0);
 
-		//get the user input
-        if (UnityEngine.Input.GetButton("Horizontal" + playerNumber))
-		{ 
-			inputDirection.x = UnityEngine.Input.GetAxis("Horizontal" + playerNumber) < 0 ? -1 : 1;
-		}
-
-        if (UnityEngine.Input.GetButton("Vertical" + playerNumber))
-        {
-            inputDirection.z = UnityEngine.Input.GetAxis("Vertical" + playerNumber) < 0 ? -1 : 1;
-        }
-
-		Vector3 direction = new(0,0,0);
-
-		//get the angle of the camera in radians
-		float angle = transform.rotation.eulerAngles.y * Mathf.PI/ 180.0f;
-
-		//rotate the direction to match with the camera direction
-		direction.x = inputDirection.x * Mathf.Cos(angle) + inputDirection.z * Mathf.Sin(angle);
-		direction.z = inputDirection.z * Mathf.Cos(angle) - inputDirection.x * Mathf.Sin(angle);
-
-        direction.Normalize();
-
-        // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-        // if there is a move input rotate player when the player is moving
-        if (direction.magnitude != 0)
-        {
-            // walk animation
-            if (anim)
-                anim.SetBool("IsMoving", true);
-        }
-        else
-        {
-            // idle animation
-            if (anim)
-                anim.SetBool("IsMoving", false);
-        }
-
-        // move the player
-        controller.Move(direction * (speed * Time.deltaTime));
     }
 
-    private void CameraRotation()
+    public void OnInteract()
     {
-		Vector2 camera_rotation = new(0,0);
+		EnterVehicle();
+    }
 
-		camera_rotation.x = UnityEngine.Input.GetAxis("Mouse Y");
-        camera_rotation.y = UnityEngine.Input.GetAxis("Mouse X");
+    private void EnterVehicle()
+	{
+        if (TryEnterVehicleInRange())
+        {
+            model.SetActive(false);
+			driving = true;
+        }
+    }
 
-		transform.eulerAngles += new Vector3(camera_rotation.x, camera_rotation.y, 0);
+	public void drive(StarterAssetsInputs input)
+	{
+		current_forklift.move(input);
+	}
+
+	public void cameraDrive(float rotation_velocity)
+	{
+		if(camera.transform.localRotation.y < 0.3f && rotation_velocity > 0)
+		{ 
+			camera.transform.Rotate(Vector3.up * rotation_velocity); 
+		}
+        else if (camera.transform.localRotation.y > -0.3f && rotation_velocity < 0)
+        {
+            camera.transform.Rotate(Vector3.up * rotation_velocity);
+        }
+    }
+
+    private void Update()
+    {
+
     }
 
     private bool TryEnterVehicleInRange()
@@ -138,6 +91,12 @@ public class PlayerController : MonoBehaviour
 		if (driveablesInRange.Count > 0)
 		{
 			driveablesInRange[0].TryEnterVehicle(this);
+			current_forklift = (ForkliftController)driveablesInRange[0];
+
+			Transform forklift_camera_root = current_forklift.getCameraRoot();
+			camera.transform.SetPositionAndRotation(forklift_camera_root.transform.position, forklift_camera_root.transform.rotation);
+			camera.transform.parent = current_forklift.transform;
+			
 			return true;
 		}
 		else
@@ -146,7 +105,44 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 	
-	/*private bool VehicleCheck()
+	public void AddVehicleInRange(IDriveable driveable)
+	{
+		// Don't add twice
+		if (!driveablesInRange.Contains(driveable))
+		{
+			driveablesInRange.Add(driveable);
+			
+			//hudManager.SetVehiclePromptStatus(playerNumber, true);
+		}
+		else
+		{
+			Debug.LogWarning("Tried to add a driveable that is already in driveablesInRange");
+		}
+	}
+	
+	public void RemoveVehicleInRange(IDriveable driveable)
+	{
+		if (driveablesInRange.Contains(driveable))
+		{
+			driveablesInRange.Remove(driveable);
+			
+			if (driveablesInRange.Count <= 0)
+			{
+				//hudManager.SetVehiclePromptStatus(playerNumber, false);
+			}
+		}
+		else
+		{
+			Debug.LogWarning("Tried to remove driveable that isn't in driveablesInRange.");
+		}
+	}
+	
+	public int GetPlayerNumber()
+	{
+		return playerNumber;
+	}
+
+    /*private bool VehicleCheck()
 	{
 		// Is there a vehicle in front of the player?
 			
@@ -177,41 +173,4 @@ public class PlayerController : MonoBehaviour
 			return false;
 		}
 	}*/
-	
-	public void AddVehicleInRange(IDriveable driveable)
-	{
-		// Don't add twice
-		if (!driveablesInRange.Contains(driveable))
-		{
-			driveablesInRange.Add(driveable);
-			
-			hudManager.SetVehiclePromptStatus(playerNumber, true);
-		}
-		else
-		{
-			Debug.LogWarning("Tried to add a driveable that is already in driveablesInRange");
-		}
-	}
-	
-	public void RemoveVehicleInRange(IDriveable driveable)
-	{
-		if (driveablesInRange.Contains(driveable))
-		{
-			driveablesInRange.Remove(driveable);
-			
-			if (driveablesInRange.Count <= 0)
-			{
-				hudManager.SetVehiclePromptStatus(playerNumber, false);
-			}
-		}
-		else
-		{
-			Debug.LogWarning("Tried to remove driveable that isn't in driveablesInRange.");
-		}
-	}
-	
-	public int GetPlayerNumber()
-	{
-		return playerNumber;
-	}
 }
