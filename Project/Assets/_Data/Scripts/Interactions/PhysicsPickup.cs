1ee98f4fdeac180d438b5734d13fd0cdda226feb
Kelvin Peach <kelvin2.peach@live.uwe.ac.uk>
2025-11-17 22:08:03 +0000

    Added Xbox icons to interaction prompts
    Added Xbox icons to interaction prompts (blue X icon instead just X text). If anyone wants to make more the links I used were: https://docs.unity3d.com/Packages/com.unity.textmeshpro@4.0/manual/Sprites.html and https://docs.unity3d.com/Packages/com.unity.textmeshpro@4.0/manual/RichTextSprite.html

--- Project/Assets/_Data/Scripts/Interactions/PhysicsPickup.cs
+++ Project/Assets/_Data/Scripts/Interactions/PhysicsPickup.cs
@@ -21,7 +21,7 @@ public class PhysicsPickup : MonoBehaviour, Pickupable
     [SerializeField]
     UnityEvent onImpactThresholdMet;
 
-    public virtual string MessageInteract => "Press X to pick up";
+    public virtual string MessageInteract => "Press <sprite name=\"Xbox_X\"> to pick up";
 
     public void Interact(InteractableControl interactableControl)
     {
