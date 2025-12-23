79714280407f458bbebfa53817654a1ffa3cee6d
Faisa-l <146775603+Faisa-l@users.noreply.github.com>
2025-12-23 14:44:13 +0000

    Fixed spelling errors

--- Project/Assets/_Data/Scripts/ScoreObject.cs
+++ Project/Assets/_Data/Scripts/ScoreObject.cs
@@ -13,11 +13,11 @@ public class ScoreObject : ScriptableObject
     /// </summary>
     public float currentScore = 0f;
 
-    [SerializeField, Range(0f, 1f), Tooltip("Score ranges for each star to be rewarded, as a percentage,")]
+    [SerializeField, Range(0f, 1f), Tooltip("Score ranges for each star to be rewarded, as a percentage.")]
     float starRangePercentage = 0.2f;
 
     /// <summary>
-    /// Stars based on the current score.
+    /// Number of stars based on the current score.
     /// </summary>
     public int Stars => GetStars();
 
@@ -27,10 +27,7 @@ public class ScoreObject : ScriptableObject
     * If we want to have variable size star ranges then this system would need to change.
     */
 
-    /// <summary>
-    /// Returns the number of stars that a given ScoreObject has.
-    /// </summary>
-    /// <param name="scoreObject"> Object to get the stars of. </param>
+    // Returns how many stars there are based on the percentage of maximum score
     private int GetStars()
     {
         float x = currentScore / maxScore;
