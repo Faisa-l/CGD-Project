0d2db5935e0dd83efcf0511507958dd1fd723f09
Kelvin Peach <kelvin2.peach@live.uwe.ac.uk>
2026-02-20 20:04:33 +0000

    Improved floating text

--- Project/Assets/_Data/Scripts/UI/FloatingText.cs
+++ Project/Assets/_Data/Scripts/UI/FloatingText.cs
@@ -6,9 +6,28 @@ public class FloatingText : MonoBehaviour
 	[Header("Settings")]
 	[SerializeField] private TMP_Text text;
 	
-	public void Setup(string newString, Color colour)
+	// Rotate to face this transform (billboard)
+	private Transform faceTarget;
+	
+	public void Setup(string newString, Color colour, Transform target)
 	{
 		text.text = newString;
 		text.color = colour;
+		faceTarget = target;
+	}
+	
+	private void Update()
+	{
+		// Look at the target, but not on the Y axis
+		// Needs to be inverted otherwise the text is backwards
+		if (faceTarget)
+		{
+			// Ignore Y axis
+			Vector3 targetPosition = faceTarget.position;
+			targetPosition.y = transform.position.y;
+			
+			// Source - https://discussions.unity.com/t/lookat-in-opposite-direction/23429
+			transform.LookAt(2 * transform.position - targetPosition);
+		}
 	}
 }
