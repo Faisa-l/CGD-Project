008156030ea21c664ef5295ed422d14966f2e3a9
Faisa-l <146775603+Faisa-l@users.noreply.github.com>
2026-03-02 22:21:51 +0000

    Randomise quota tags in collection schedules
    Done by a new function which randomises just the tags for quotas. Also moved the ShuffleList function from the crate spawner class to crate extensions.

--- Project/Assets/_Data/Scripts/Crates/CrateSpawner.cs
+++ Project/Assets/_Data/Scripts/Crates/CrateSpawner.cs
@@ -97,20 +97,6 @@ public class CrateSpawner : MonoBehaviour
     void SpawnCrate(in Transform point, in SpawnRequirements requirement) 
         => requirement.instances[point] = CrateObject.Instantiate(cratePrefab, point, requirement.tag, requirement.damageBehaviour, requirement.crateScore);
 
-    // Randomise spawnable transforms (Fisher-Yates shuffle I found on stack overflow)
-    // Partition list from 0 to pointer to end -> Select random element -> swap with pointer element -> decrement pointer
-    static void ShuffleList<T>(List<T> list)
-    {
-        var rnd = new System.Random();
-        int n = list.Count;
-        while (n > 1)
-        {
-            n--;
-            int k = rnd.Next(0, n + 1);
-            (list[n], list[k]) = (list[k], list[n]);
-        }
-    }
-
     // Draws the spawner locations and what colour they are for. Size is also based on the score 
     private void OnDrawGizmosSelected()
     {
