c6ddc5fb19aa0db5132be70e3151541f54ce5897
Nathan <nghaigh04@icloud.com>
2026-03-03 15:05:28 +0000

    FixedKillVol

--- Project/Assets/_Data/Scripts/KillVolume.cs
+++ Project/Assets/_Data/Scripts/KillVolume.cs
@@ -28,20 +28,7 @@ public class KillVolume : MonoBehaviour
         if(other.tag == "Player")
         {
             var player_character = other.gameObject.transform;
-            player_character.GetComponent<CharacterController>().enabled = false;
             player_character.transform.position = respawn_pos.position;
-            player_character.GetComponent <CharacterController>().enabled = true;   
-        }
-        else if (other.tag == "Forklift")
-        {
-            var forklift = other.gameObject.transform.parent.gameObject.transform.parent.gameObject.transform.parent.gameObject;
-            forklift.GetComponent<CharacterController>().enabled = false;
-            forklift.transform.position = respawn_pos.position;
-            forklift.GetComponent<CharacterController>().enabled = true;
-        }
-        else
-        {
-            other.transform.position = respawn_pos.position;
         }
     }
 }
